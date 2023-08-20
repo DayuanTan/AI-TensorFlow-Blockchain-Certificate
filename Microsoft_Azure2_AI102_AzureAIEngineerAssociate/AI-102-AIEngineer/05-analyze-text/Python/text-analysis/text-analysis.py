a5ffd1d8cac3eeaa3cd514546b78470eadab1504
BigCircle <zhuceafter2015@163.com>
2023-08-20 18:00:26 -0400

    add ai102 cert

--- Microsoft_Azure2_AI102_AzureAIEngineerAssociate/AI-102-AIEngineer/05-analyze-text/Python/text-analysis/text-analysis.py
+++ Microsoft_Azure2_AI102_AzureAIEngineerAssociate/AI-102-AIEngineer/05-analyze-text/Python/text-analysis/text-analysis.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 import os
 
 # Import namespaces
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
 
 
 def main():
@@ -12,6 +14,8 @@ def main():
         cog_key = os.getenv('COG_SERVICE_KEY')
 
         # Create client using endpoint and key
+        credential = AzureKeyCredential(cog_key)
+        cog_client = TextAnalyticsClient(endpoint=cog_endpoint, credential=credential)
 
 
         # Analyze each text file in the reviews folder
@@ -23,18 +27,38 @@ def main():
             print('\n' + text)
 
             # Get language
+            detectedLanguage = cog_client.detect_language(documents=[text])[0]
+            print('\nLanguage: {}'.format(detectedLanguage.primary_language.name))
 
 
             # Get sentiment
+            sentimentAnalysis = cog_client.analyze_sentiment(documents=[text])[0]
+            print("\nSentiment: {}".format(sentimentAnalysis.sentiment))
 
 
             # Get key phrases
+            phrases = cog_client.extract_key_phrases(documents=[text])[0].key_phrases
+            if len(phrases) > 0:
+                print("\nKey Phrases:")
+                for phrase in phrases:
+                    print('\t{}'.format(phrase))
+
 
 
             # Get entities
+            entities = cog_client.recognize_entities(documents=[text])[0].entities
+            if len(entities) > 0:
+                print("\nEntities")
+                for entity in entities:
+                    print('\t{} ({})'.format(entity.text, entity.category))
 
 
             # Get linked entities
+            entities = cog_client.recognize_linked_entities(documents=[text])[0].entities
+            if len(entities) > 0:
+                print("\nLinks")
+                for linked_entity in entities:
+                    print('\t{} ({})'.format(linked_entity.name, linked_entity.url))
 
 
 
---
+++ Microsoft_Azure2_AI102_AzureAIEngineerAssociate/AI-102-AIEngineer/05-analyze-text/test.md
@@ -0,0 +1,24 @@
+Knowledge check
+Completed
+200 XP
+3 minutes
+
+1. How should you create an application that monitors the comments on your company's web site and flags any negative posts? 
+
+Use the Language service to extract key phrases.
+
+Use the Language service to perform sentiment analysis of the comments.
+Correct. sentiment analysis helps you determine if text is negative or positive.
+
+Use the Language service to extract named entities from the comments.
+
+
+
+2. You have analyzed text that contains the word "Paris". How might you determine of this word refers to the French city or the character in Homer's "The Iliad"? 
+
+Use the Language service to extract key phrases.
+
+Use the Language service to detect the language of the text.
+
+Use the Language service to extract linked entities.
+Correct. Linked entities enable you to disambiguate common entities of the same name.
